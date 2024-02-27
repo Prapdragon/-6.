700f5c32892994639c813a8e3173c6421a739894
Prapdragon <139678437+Prapdragon@users.noreply.github.com>
2024-02-27 16:01:39 +0700

    Add files via upload

---
+++ # 4.py
@@ -0,0 +1,10 @@
+a={10, 20, 30, 40, 50, 60}
+b={30, 40, 50, 60, 70, 80}
+set1=b.union(a)
+print(set1)
+set2=b.intersection(a)
+print(set2)
+set3=a.difference(b)
+print(set3)
+set4=a.symmetric_difference(b)
+print(set4)
